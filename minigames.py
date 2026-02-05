00ed92b7e669b4ad30358ed9b807e814d89cc05d
3rror886 <viniciusaugusto1006@gmail.com>
2026-02-05 20:05:56 -0300

    Update minigames.py

--- minigames.py
+++ minigames.py
@@ -17,14 +17,15 @@ print("Você escuta uma movimentação em um arbusto próximo, mas quando você 
 escolha = input("Oh não criatura apareceu, oque fará Herói? \ndigite A para atacar \ndigite D para defender \ndigite M para lançar uma magia poderosa \ndigite F para gritar e fugir \n")
 
 match escolha:
-    case "A":
+    case "A" | "a":
         print("Você ataca com sua espada, assim você Derrota o monstro")
-    case "D":
+    case "D" | "d": 
         print("Você defende incasavelmente, passam-se 3 dias e 3 noites, até que sua batalha de resistência termina, e a criatura desmaia de exaustão, espero que tenha valido apena Heroi")
-    case "M":
+    case "M" | "m":
         print("Você lança a sua mais nova magia com o seu cajado, uma bola de fogo desce do céu destruindo seu oponente..... e você, a floresta e a casa de um lenhador que morava por ali, descanse em paz Heroi")
-    case "F":
+    case "F" | "f":
         print("Você corre com se sua vida dependesse disso, e assim o conto do Heroi covarde se espalha por toda parte")
     case _:
 
         print("você comprou o jogo foi, o meu eo seu...")
+
